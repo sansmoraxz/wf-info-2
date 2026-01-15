9ce28e306522eb5a411de0e1ef420420d34b7151
Souyama <souyamadebnath@gmail.com>
2026-01-15 19:56:25 +0530

    Refactor date deserialization to improve error handling and remove unused imports

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
-use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
-use serde::{Deserialize, Deserializer, Serialize};
+use chrono::{DateTime, TimeZone, Utc};
+use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 
 pub fn deserialize_mongo_date_option<'de, D>(
@@ -15,13 +15,21 @@ where
 
     if let Value::Number(n) = &v {
         if let Some(ms) = n.as_i64() {
-            return Ok(Some(ms_to_dt(ms)));
+            if let Some(dt) = ms_to_dt(ms) {
+                return Ok(Some(dt));
+            } else {
+                return Err(serde::de::Error::custom("invalid timestamp"));
+            }
         }
     }
 
     if let Value::String(s) = &v {
         if let Ok(ms) = s.parse::<i64>() {
-            return Ok(Some(ms_to_dt(ms)));
+            if let Some(dt) = ms_to_dt(ms) {
+                return Ok(Some(dt));
+            } else {
+                return Err(serde::de::Error::custom("invalid timestamp"));
+            }
         }
         if let Ok(dt) = DateTime::parse_from_rfc3339(s) {
             return Ok(Some(dt.with_timezone(&Utc)));
@@ -31,17 +39,29 @@ where
     if let Value::Object(map) = &v {
         if let Some(Value::String(num_s)) = map.get("$numberLong") {
             if let Ok(ms) = num_s.parse::<i64>() {
-                return Ok(Some(ms_to_dt(ms)));
+                if let Some(dt) = ms_to_dt(ms) {
+                    return Ok(Some(dt));
+                } else {
+                    return Err(serde::de::Error::custom("invalid timestamp"));
+                }
             }
         }
         if let Some(Value::Number(num)) = map.get("$numberLong") {
             if let Some(ms) = num.as_i64() {
-                return Ok(Some(ms_to_dt(ms)));
+                if let Some(dt) = ms_to_dt(ms) {
+                    return Ok(Some(dt));
+                } else {
+                    return Err(serde::de::Error::custom("invalid timestamp"));
+                }
             }
         }
         if let Some(Value::String(s)) = map.get("$date") {
             if let Ok(ms) = s.parse::<i64>() {
-                return Ok(Some(ms_to_dt(ms)));
+                if let Some(dt) = ms_to_dt(ms) {
+                    return Ok(Some(dt));
+                } else {
+                    return Err(serde::de::Error::custom("invalid timestamp"));
+                }
             }
             if let Some(Value::String(ds)) = map.get("$date") {
                 if let Ok(dt) = DateTime::parse_from_rfc3339(ds) {
@@ -54,10 +74,9 @@ where
     Err(serde::de::Error::custom("unsupported date format"))
 }
 
-fn ms_to_dt(ms: i64) -> DateTime<Utc> {
+fn ms_to_dt(ms: i64) -> Option<DateTime<Utc>> {
     let secs = ms / 1000;
     let nsec = ((ms % 1000).abs() as u32) * 1_000_000;
     Utc.timestamp_opt(secs, nsec)
         .single()
-        .unwrap_or_else(|| DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc))
 }
